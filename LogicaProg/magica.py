415842b9b1ada76de08991c0586885524ea70a8c
raylla <rayllarsd@gmail.com>
2026-01-14 22:37:23 -0400

    dois

--- LogicaProg/magica.py
+++ LogicaProg/magica.py
@@ -1,6 +1,13 @@
+import os
+
+os.system("clear")
+
 print("Olhe as cartas e escolha uma:")
 print("----")
 print("Q♣️ K♦️ J♥️ Q♥️ J♣️ K♠️")
+
+os.system("clear")
+
 
 print("A carta que vc pensou...")
 
