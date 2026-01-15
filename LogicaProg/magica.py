fafe8b4ee0f6c2ff71fd0d14ca72a8f1b8ef7074
raylla <rayllarsd@gmail.com>
2026-01-14 22:41:54 -0400

    magic

--- LogicaProg/magica.py
+++ LogicaProg/magica.py
@@ -5,14 +5,18 @@ os.system("clear")
 print("Olhe as cartas e escolha uma:")
 print("----")
 print("Q♣️ K♦️ J♥️ Q♥️ J♣️ K♠️")
+input()
 
 os.system("clear")
 
 
 print("A carta que vc pensou...")
+input()
 
 print()
 print("Desapareceu!")
 print()
 
 print("J♠️ Q♦️ J♦️ Q♠️ K♥️")
+input()
+
