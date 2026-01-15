dfdccfcfdcc6f8ee1bf030d814731f2357084709
raylla <rayllarsd@gmail.com>
2026-01-14 22:07:59 -0400

    one

---
+++ LogicaProg/magica.py
@@ -0,0 +1,11 @@
+print("Olhe as cartas e escolha uma:")
+print("----")
+print("Q♣️ K♦️ J♥️ Q♥️ J♣️ K♠️")
+
+print("A carta que vc pensou...")
+
+print()
+print("Desapareceu!")
+print()
+
+print("J♠️ Q♦️ J♦️ Q♠️ K♥️")
--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # inicio
-fim
-um
-dois
